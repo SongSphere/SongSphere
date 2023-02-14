d1c144ae329e8d987f80925c022c5298f60ac5f3
Chi-Wei Lien <wilsonwilson3124@gmail.com>
2023-02-13 22:26:32 -0500

    set up server with express and typescript

---
+++ server/src/server.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+
+const app = express();
+
+const port = process.env.PORT || "8080";
+
+
+app.get('/', (req, res) => {
+    res.send('Welcome to the backend!');
+})
+
+app.listen(port, () => {
+    console.log(`Server started at http://localhost:${port}`);
+  });
