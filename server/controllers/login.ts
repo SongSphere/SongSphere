ef7fe15fa18dd817c4cdbf13df40e8e2fdf75afa
Chi-Wei Lien <wilsonwilson3124@gmail.com>
2023-02-14 17:58:38 -0500

    separate create user to a function in services

--- server/controllers/login.ts
+++ server/controllers/login.ts
@@ -6,21 +6,12 @@ import User from "../db/user";
 
 // import services
 import { validateToken } from "../services/google-login";
-import { saveUser } from "../services/db";
+import { createUser, saveUser } from "../services/db";
 
 export const login = (req: Request, res: Response, next: NextFunction) => {
   const { token } = req.body;
   validateToken(token).then(async (userData) => {
-    const user = new User({
-      name: userData.name,
-      givenName: userData.given_name,
-      familyName: userData.family_name,
-      email: userData.email,
-      emailVerified: userData.email_verified,
-      profileImgUrl: userData.picture,
-      token: token,
-    });
-
-    saveUser(user);
+    const user = createUser(userData, token);
+    saveUser(await user);
   });
 };
--- server/services/db.ts
+++ server/services/db.ts
@@ -1,8 +1,25 @@
 // import packages
+import { TokenPayload } from "google-auth-library";
 import mongoose, { ObjectId } from "mongoose";
 
 // import models
-import { IUser } from "../db/user";
+import User, { IUser } from "../db/user";
+
+export const createUser = async (
+  userData: TokenPayload,
+  token: string
+): Promise<mongoose.Document<unknown, any, IUser>> => {
+  const user = new User({
+    name: userData.name,
+    givenName: userData.given_name,
+    familyName: userData.family_name,
+    email: userData.email,
+    emailVerified: userData.email_verified,
+    profileImgUrl: userData.picture,
+    token: token,
+  });
+  return user;
+};
 
 export const saveUser = async (
   user: mongoose.Document<unknown, any, IUser>
