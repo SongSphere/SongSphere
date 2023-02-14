1636ee189f4ff4131381f61943c7eef0245c3a90
Chi-Wei Lien <wilsonwilson3124@gmail.com>
2023-02-14 11:54:24 -0500

    create dummy component for login

---
+++ client/src/services/handle-login.ts
@@ -0,0 +1,19 @@
+import React, { Dispatch } from "react";
+import { CredentialResponse } from "@react-oauth/google";
+
+const handleLogin = async (credentialResponse: CredentialResponse) => {
+  await fetch(`${process.env.REACT_APP_API}/api/auth/google`, {
+    method: "POST",
+    credentials: "include",
+    body: JSON.stringify({
+      token: credentialResponse.credential,
+    }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  }).then(async () => {
+    // do things after login
+  });
+};
+
+export default handleLogin;
