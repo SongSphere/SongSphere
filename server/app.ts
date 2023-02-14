5e9ed6a627a4181474f2a401da42fd88caace24b
Chi-Wei Lien <wilsonwilson3124@gmail.com>
2023-02-14 10:48:50 -0500

    set up cors policy and bodyparser

--- server/app.ts
+++ server/app.ts
@@ -2,13 +2,26 @@
 import * as dotenv from "dotenv";
 dotenv.config();
 
+// import package
 import express from "express";
+import cors from "cors";
 import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
 
+// import routers
 import sampleRouter from "./routes/sample";
 
 const app = express();
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
 app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 // set routers
 app.use(sampleRouter);
@@ -16,5 +29,5 @@ app.use(sampleRouter);
 const port = process.env.PORT || "8080";
 
 app.listen(port, () => {
-    console.log(`Server started at http://localhost:${port}`);
+  console.log(`Server started at http://localhost:${port}`);
 });
