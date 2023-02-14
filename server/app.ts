fd43cc15603e852d5fc73c61ec8fac17e154e556
Chi-Wei Lien <wilsonwilson3124@gmail.com>
2023-02-14 10:06:49 -0500

    remvoe node_modules and add server structure folders

--- server/app.ts
+++ server/app.ts
@@ -1,14 +1,19 @@
+import * as dotenv from "dotenv";
+dotenv.config();
+
 import express from 'express';
+import cookieParser from "cookie-parser";
 
 const app = express();
+app.use(cookieParser());
 
 const port = process.env.PORT || "8080";
 
 
 app.get('/', (req, res) => {
     res.send('Welcome to the backend!');
-})
+});
 
 app.listen(port, () => {
     console.log(`Server started at http://localhost:${port}`);
-  });
+});
