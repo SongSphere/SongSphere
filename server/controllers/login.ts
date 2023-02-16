4c0a7796218ca39726a86a009d9daf982ac4c259
Chi-Wei Lien <wilsonwilson3124@gmail.com>
2023-02-15 20:12:52 -0500

    create dummy api endpoint to test middleware

--- server/controllers/login.ts
+++ server/controllers/login.ts
@@ -1,9 +1,5 @@
 // import packages
 import { Request, Response, NextFunction } from "express";
-import mongoose, { Document } from "mongoose";
-
-// import db
-import User, { IUser } from "../db/user";
 
 // import services
 import { validateToken } from "../services/google-login";
@@ -13,36 +9,6 @@ import {
   checkUser,
   updateUserToken,
 } from "../services/db";
-import { rmSync } from "fs";
-
-// export const loginV2 = (
-//   User: mongoose.Model<IUser, {}, {}, {}, any>,
-//   customRequest: Request,
-//   customResponse: Response
-// ) => {
-//   return async (req: Request, res: Response, next: NextFunction) => {
-//     if (customRequest == undefined) customRequest = req;
-//     if (customResponse == undefined) customResponse = res;
-
-//     const { token } = customRequest.body;
-//     try {
-//       const userData = await validateToken(token);
-//       const exist = await checkUser(userData.email, User);
-//       if (exist) {
-//         // exist == document with _id
-//         await updateUserToken(userData.email, token, User);
-//       } else {
-//         // exist == null
-//         const user = await createUser(userData, token, User);
-//         await saveUser(user);
-//       }
-//     } catch (error) {
-//       console.error(error);
-//       customResponse.status(500);
-//       customResponse.json({ error: error });
-//     }
-//   };
-// };
 
 export const login = async (
   req: Request,
@@ -79,4 +45,8 @@ export const login = async (
     res.status(500);
     res.json({ error: error });
   }
+};
+
+export const testauth = (req: Request, res: Response) => {
+  res.send("welcome. you are logged in");
 };
--- server/routes/login.ts
+++ server/routes/login.ts
@@ -1,10 +1,15 @@
+// import packages
 import express from "express";
-import { login } from "../controllers/login";
 
-import User from "../db/user";
+// import controllers
+import { login, testauth } from "../controllers/login";
+
+// import middleware
+import auth from "../middleware/auth";
 
 const router = express.Router();
 
 router.post("/api/auth/google", login);
+router.get("/api/testauth", auth, testauth);
 
 export default router;
