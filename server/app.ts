e9493510c97e5a5c4d5adc341826a114e7e68482
Chi-Wei Lien <wilsonwilson3124@gmail.com>
2023-02-15 20:48:17 -0500

    store user session on mongodb

--- server/app.ts
+++ server/app.ts
@@ -9,6 +9,10 @@ import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
 import session from "express-session";
 
+import { default as connectMongoDBSession } from "connect-mongodb-session";
+
+const MongoDBStore = connectMongoDBSession(session);
+
 // import routers
 import sampleRouter from "./routes/sample";
 import loginRouter from "./routes/login";
@@ -34,13 +38,25 @@ app.use(
   })
 );
 app.use(cookieParser());
+
+const username = process.env.DB_USER_NAME;
+const password = process.env.DB_PASSWORD;
+const cluster = process.env.DB_CLUSTER;
+
 app.use(
   session({
     name: "ssid",
     secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: false,
-    cookie: { secure: process.env.NODE_ENV === "production" },
+    cookie: {
+      secure: process.env.NODE_ENV === "production",
+      maxAge: 1000 * 60 * 60 * 24, // 1 day
+    },
+    store: new MongoDBStore({
+      uri: `mongodb+srv://${username}:${password}@${cluster}.1oxopys.mongodb.net/main?retryWrites=true&w=majority`,
+      collection: "sessions",
+    }),
   })
 );
 app.use(bodyParser.urlencoded({ extended: true }));
