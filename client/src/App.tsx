da3908eee433fd9f5e719cc424c25077e85e9d82
committ <89640845+spaceComplexitys@users.noreply.github.com>
2023-02-21 16:55:05 -0500

    Design based on mock finished

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,19 @@
 import React from "react";
 import DummyLogin from "./components/dummy-login";
+import SideBar from "./components/size-bar";
+import AuthPage from "./pages/auth-page";
+
 
 function App() {
   return (
     <div>
-      <p>default starting component</p>
-      <DummyLogin />
+       {/* <SideBar/> */}
+       <AuthPage/>
     </div>
+    // <div>
+    //   <p>default starting component</p>
+    //   <DummyLogin />
+    // </div>
   );
 }
 
--- client/src/pages/auth-page.tsx
+++ client/src/pages/auth-page.tsx
@@ -10,13 +10,14 @@ const AuthPage = () => {
     <div className="flex h-screen w-screen place-items-center justify-center bg-background">
       <div className="flex h-4/5 w-4/5 flex-row rounded-xl bg-white">
         <div className="flex w-1/2 flex-col justify-center text-center">
-          <div className="">hi</div>
-          <div>hi</div>
-          <div>
-            <button className="w-fit px-2"><DummyLogin/></button>
+          <div className="text-4xl font-bold justify-center">Welcome back </div>
+          <div className="text-center font-light pr-20">Continue With Google</div>
+          <div className="pr-14">
+            <button className="w-fit pt-6 px-8"><DummyLogin/></button>
           </div>
         </div>
-        <div className="w-1/2 rounded-xl bg-gray-200 bg-[url('https://lh3.googleusercontent.com/a/AEdFTp6LDtlFlsOSWZstQy1jaYLjDcje3Y4uDY3VNXuR=s96-c')]"></div>
+        {/* <div className="w-1/2 rounded-xl bg-gray-200 bg-[url('https://wallpapercave.com/wp/wp6871302.png')]"></div> */}
+        <div className="w-1/2 rounded-xl bg-gradient-to-r from-violet-500 to-fuchsia-500" ></div>
       </div>
     </div>
   );
