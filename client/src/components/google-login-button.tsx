1b54df52906dbd7bd2e9f226d16a3df6c4099f9a
Chi-Wei Lien <wilsonwilson3124@gmail.com>
2023-02-25 10:17:38 -0500

    remove unnecessary console logs

--- client/src/components/google-login-button.tsx
+++ client/src/components/google-login-button.tsx
@@ -8,19 +8,15 @@ import handleSignInUp from "../services/handle-sign-in-up";
 const LoginButton = () => {
   // you can use the isLoggedIn with useContext to see if the user is signed in
   const { isLoggedIn, setIsLoggedIn } = useContext(isLoggedInContext);
-  console.log("originally: " + isLoggedIn);
   return (
     <div>
       <GoogleLogin
         onSuccess={async (credentialResponse) => {
           const loginSuccess = await handleSignInUp(credentialResponse);
-          console.log("logging in");
-          console.log(loginSuccess);
           if (loginSuccess) {
             // login success
             // redirect to home page
             setIsLoggedIn(true);
-            console.log("after: " + isLoggedIn);
           }
           // login failed
         }}
