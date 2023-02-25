55c55ee22531e07b6eb4c2e42c35df96daa9606d
anthonyfmb <anthony.baumann@icloud.com>
2023-02-24 19:19:02 -0500

    Apple Music Returns specified songs, albums and artists

--- client/src/components/apple-search.tsx
+++ client/src/components/apple-search.tsx
@@ -2,71 +2,102 @@
   This is a temporary test component for linking to Apple Music API
 */
 
-import React from "react";
+import React, { useState } from "react";
 
 // import services
 import { useEffect } from "react";
 import styled from "styled-components";
 import search from "../services/apple-search";
 
-const useScript = (url: string) => {
-  useEffect(() => {
-    const script = document.createElement("script");
-
-    script.src = url;
-    script.async = true;
-
-    document.body.appendChild(script);
-
-    return () => {
-      document.body.removeChild(script);
-    };
-  }, [url]);
-};
-
 const AppleSearch = () => {
+  //let songs: [string, string][] = useState([]);
+  let [songs, setSongs] = useState<any[]>([]);
+  let [selected, setSelected] = useState<string[]>([]);
+  let [song, setSong] = useState<string>("");
+  let [category, setCategory] = useState<string>("songs");
+  const [open, setOpen] = React.useState(false);
+
+  const handleOpen = () => {
+    setOpen(!open);
+  };
+
   return (
     <div>
-      {/* <form onSubmit={(event) => search(event.target. as string, "songs")}>
-        {/* <label>
-          Name:
-          <input type="text" value={this.state.value} onChange={this.handleChange} />
-        </label>
-        <label> */}
       <input
         placeholder="Enter Post Title"
-        onChange={(event) => search(event.target.value as string, "songs")}
+        onChange={(event) =>
+          search(event.target.value as string, category).then((result) => {
+            setSong(event.target.value);
+            songs = result!;
+            setSongs(result!);
+          })
+        }
       />
+      <div className="dropdown">
+        <button onClick={handleOpen}>Dropdown</button>
+        {open ? (
+          <ul className="menu">
+            <li className="songs">
+              <button
+                onClick={() =>
+                  search(song as string, "songs").then((result) => {
+                    setCategory("songs");
+                    console.log(result);
+                    songs = result!;
+                    setSongs(result!);
+                  })
+                }
+              >
+                Songs
+              </button>
+            </li>
+            <li className="albums">
+              <button
+                onClick={() =>
+                  search(song as string, "albums").then((result) => {
+                    setCategory("albums");
+                    console.log(result);
+                    songs = result!;
+                    setSongs(result!);
+                  })
+                }
+              >
+                Albums
+              </button>
+            </li>
+            <li className="artists">
+              <button
+                onClick={() =>
+                  search(song as string, "artists").then((result) => {
+                    setCategory("artists");
+                    console.log(result);
+                    songs = result!;
+                    setSongs(result!);
+                  })
+                }
+              >
+                Artists
+              </button>
+            </li>
+          </ul>
+        ) : null}
+      </div>
       <button data-apple-music-skip-to-previous-item></button>
-      {/* </label>
-        <input type="submit" value="Submit" />
-      </form> */}
-      {/* <apple-music-artwork
-      alt="Fearless (Taylor's Version)"
-      source="https://is2-ssl.mzstatic.com/image/thumb/Music124/v4/e0/d7/50/e0d750e8-b290-a8ff-88fe-8cbed749120c/21UMGIM09915.rgb.jpg/{w}x{h}bb.{f}"
-      width="250"
-      ></apple-music-artwork> */}
       <h1 id="name"></h1>
       <script>
         const artworkElement = document.querySelector('apple-music-artwork');
         artworkElement.source = album.attributes.artwork;
       </script>
+      {songs.map(([song, id]) => (
+        <div>
+          <button key={id} onClick={() => setSelected([song, id])}>
+            {song}
+          </button>
+        </div>
+      ))}
+      <div>Selected: {selected}</div>
     </div>
   );
 };
-
-// const AppleSearch = () => {
-//   return (
-//     <div>
-//       <h1>Adding 'a' and 'b'</h1>
-//       <form>
-//         a: <input type="number" name="a" id="a"><br>
-//         b: <input type="number" name="b" id="b"><br>
-//         <Button onClick={search(a, b)}>Add</Button>
-//         <Button onClick={setUp}>Authorize Apple Music</Button>
-//       </form>
-//     </div>
-//   );
-// };
 
 export default AppleSearch;
--- client/src/services/apple-search.ts
+++ client/src/services/apple-search.ts
@@ -2,7 +2,8 @@ import React, { Dispatch } from "react";
 import { textChangeRangeNewSpan } from "typescript";
 
 const search = async (term: string, types: string) => {
-  if (term === "") return;
+  const list: [string, string][] = [];
+  if (term === "") return list;
 
   const music = MusicKit.getInstance();
 
@@ -14,30 +15,12 @@ const search = async (term: string, types: string) => {
     offset: 0,
   });
 
-  //console.log(Object.keys(response));
-
   const songs = Object.values(response)[0].data;
   songs.forEach(function (entry: any) {
-    console.log(term);
-    console.log(entry.attributes.name);
-    //document.getElementsByName("name").innerHTML = entry;
+    list.push([entry.attributes.name, entry.id]);
   });
-  //console.log(Object.values(response)[0].data[0]);
-  //   response.forEach(function (entry) {
-  //     console.log(entry);
-  //   });
-  const res = response[1] as MusicKit.Resource;
-  //console.log(typeof response[1]);
 
-  //   const result = await music.api.music(`/v1/catalog/us/search`, {
-  //     term: term,
-  //     types: types,
-  //   });
-
-  //term: string, parameters?: QueryParameters
-  //   console.log(response);
-  //   console.log(response[0]);
-  //   console.log(response.get("songs"));
+  return list;
 };
 
 export default search;
