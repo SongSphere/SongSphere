cb1ac7a2451675ced3a30c9e67c4407def3e8a02
Chi-Wei Lien <wilsonwilson3124@gmail.com>
2023-02-26 14:35:07 -0500

    create play pause button

--- client/src/components/music-player-card.tsx
+++ client/src/components/music-player-card.tsx
@@ -1,4 +1,12 @@
+import { useState } from "react";
+
 const MusicPlayerCard = () => {
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  const playMusicHander = () => {
+    setIsPlaying(!isPlaying);
+  };
+
   return (
     <div className="relative flex justify-center h-screen">
       <div className="fixed flex h-full mt-8">
@@ -9,6 +17,22 @@ const MusicPlayerCard = () => {
             </div>
           </div>
           <div className="mt-2 text-2xl text-center">Into the Night</div>
+          <div className="flex justify-center mt-2">
+            <div
+              className="w-5 h-5 cursor-pointer"
+              onClick={() => {
+                playMusicHander();
+              }}
+            >
+              <img
+                src={
+                  isPlaying
+                    ? "/img/icons/pause-icon.png"
+                    : "/img/icons/play-icon.png"
+                }
+              ></img>
+            </div>
+          </div>
         </div>
       </div>
     </div>
