cc6844d54007ac9ba84681b421f1b115080a3c2f
Chi-Wei Lien <wilsonwilson3124@gmail.com>
2023-02-27 16:30:09 -0500

    update home page

--- client/src/pages/home-page.tsx
+++ client/src/pages/home-page.tsx
@@ -1,6 +1,8 @@
 import { useContext, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import AppleLink from "../components/apple-link";
+import MusicPlayerCard from "../components/music-player-card";
+import NewNavbar from "../components/new-navbar";
 import SpotfiyLinkButton from "../components/spotify-link";
 import { userSessionContext } from "../context/userSessionContext";
 import handleSignout from "../services/handle-sign-out";
@@ -14,25 +16,33 @@ const HomePage = (props: IHomePageProps) => {
     useContext(userSessionContext);
 
   return (
-    <div>
-      <div>Home</div>
-      <AppleLink musicInstance={props.musicInstance} />
-      <SpotfiyLinkButton />
-      <div>
-        <button
-          onClick={async () => {
-            // If sign out success then set loggedin to false
-            // Redirect to the authentication page
-            const logoutSuccesss = await handleSignout();
-            if (logoutSuccesss) {
-              setUser(null);
-              setIsLoggedIn(false);
-              window.location.reload();
-            }
-          }}
-        >
-          logout
-        </button>
+    <div className="w-full h-full min-h-screen min-w-screen bg-slate-100">
+      <NewNavbar />
+      <div className="grid grid-cols-4 gap-8">
+        <div>maybe friend activities</div>
+        <div className="col-span-2">
+          <div>temporary setting stuff:</div>
+          <AppleLink musicInstance={props.musicInstance} />
+          <SpotfiyLinkButton />
+          <div>
+            <button
+              className="p-2 rounded-md bg-amber-400"
+              onClick={async () => {
+                // If sign out success then set loggedin to false
+                // Redirect to the authentication page
+                const logoutSuccesss = await handleSignout();
+                if (logoutSuccesss) {
+                  setUser(null);
+                  setIsLoggedIn(false);
+                  window.location.reload();
+                }
+              }}
+            >
+              logout
+            </button>
+          </div>
+        </div>
+        <MusicPlayerCard musicInstance={props.musicInstance} />
       </div>
     </div>
   );
