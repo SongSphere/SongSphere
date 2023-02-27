93b41e550f7c19fbc35959502e1d9bc45c49f407
anthonyfmb <anthony.baumann@icloud.com>
2023-02-26 21:17:24 -0500

    remove sorry

--- client/src/pages/post-page.tsx
+++ client/src/pages/post-page.tsx
@@ -1,27 +1,19 @@
 import React from "react";
 import ReactDOM from "react";
-import handleSumbitPost from "../services/handle-submit-post";
 import Search from "../components/search";
 
 const PostPage = () => {
-    return (
+  return (
     //page container
     <div className="grid justify-center w-screen h-screen grid-cols-4 grid-rows-4 bg-navy">
-        <div className="grid grid-cols-3 col-start-2 col-end-4 row-start-1 row-end-4 rounded-lg bg-lgrey">
-            <div className="col-span-2 rounded-lg">
-            <Search />
-                
-            
-                
-                
-            </div>
-            <div className="col-span-1 rounded-lg">
-                
-            </div>
+      <div className="grid grid-cols-3 col-start-2 col-end-4 row-start-1 row-end-4 rounded-lg bg-lgrey">
+        <div className="col-span-2 rounded-lg">
+          <Search />
         </div>
+        <div className="col-span-1 rounded-lg"></div>
+      </div>
     </div>
-      
-    );
+  );
 };
 
 export default PostPage;
