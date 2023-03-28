468022a6ac40833b7ff2fcec39b3650f9195ad34
ekane02 <70538006+ekane02@users.noreply.github.com>
2023-03-28 14:57:09 -0400

    view likes page fully functional

--- client/src/pages/likes-page.tsx
+++ client/src/pages/likes-page.tsx
@@ -9,11 +9,9 @@ import LikeFeed from "../components/profile/likes-feed";
 import { TMusicContent } from "../types/music-content";
 
 const LikesPage = () => {
-    const [posts, setPosts] = useState<String[]>([]);
+    const [posts, setPosts] = useState<TPost[]>([]);
     const [user, setUser] = useState<TUser | null>(null);
-    const [tpost, setTpost ] = useState<TPost[]>([]);
     const [song, setSong] = useState<TMusicContent | null>(null);
-    let ids:TPost[];
     useEffect(() => {
         setUser(Session.getUser());
       }, [Session.getUser()]);
@@ -21,28 +19,22 @@ const LikesPage = () => {
         if (user) {
           fetchLikesByUsername(user.username).then((posts) => {
             setPosts(posts);
-          }).then(() => {
-            for(let i = 0; i < posts.length; i++) {
-              if(posts[i]) {
-                fetchPostById(posts[i].toString()).then((tposts) => {
-                  ids.push(tposts);
-                });
-                setTpost(ids);
-              }
-            }
-          });
+          })
           
         }
       }, [user]);
+      
       if (!user) {
         return <div>fetching user</div>;
       }
       
     return (
-        <div className="w-screen h-screen text-center bg-lblue">
-            <h1 className="text-4xl text-white">Liked Posts</h1>
-            <div>
-                <LikeFeed user={user} posts={tpost} setSong={setSong}/>
+        <div className="w-full h-full min-h-screen text-center min-w-screen bg-lblue">
+          <h1 className="text-4xl text-center text-white">Liked Posts</h1>
+            <div className="grid grid-cols-4 gap-7 md:grid-flow-col">
+              <div className="col-span-2 col-start-2">
+                  <LikeFeed user={user} posts={posts} setSong={setSong}/>
+              </div>
             </div>
         </div>
     )
--- client/src/services/posts/fetch-user-likes.ts
+++ client/src/services/posts/fetch-user-likes.ts
@@ -1,7 +1,8 @@
 import { TUser } from "../../types/user";
+import { TPost } from "../../types/post";
 
 const fetchLikesByUsername = async (username: string) => {
-    return new Promise<String[]>(async (resolve, reject) => {
+    return new Promise<TPost[]>(async (resolve, reject) => {
       await fetch(`${process.env.REACT_APP_API}/api/user/fetchLikedPosts/${username}`, {
         method: "GET",
         credentials: "include",
