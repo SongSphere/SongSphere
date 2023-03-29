b2a003aa4a35abcbdb5d180001502b23dee7a280
anthonyfmb <anthony.baumann@icloud.com>
2023-03-29 19:07:27 -0400

    added api apple music fetch

--- client/src/pages/recents-page.tsx
+++ client/src/pages/recents-page.tsx
@@ -10,12 +10,13 @@ import RecentFeed from "../components/profile/recents-feed";
 import AppleMusicPlayerCard from "../components/player/apple-music-player-card";
 import SpotifyPlayerCard from "../components/player/spotify-music-player-card";
 import { getSpotifyRecentlyPlayedSongs } from "../services/spotify/spotify-recently-played";
+import { getAppleRecentlyPlayedSongs } from "../services/apple/apple-recently-played";
 
 interface IRecentProps {
   appleMusicInstance: MusicKit.MusicKitInstance;
 }
 
-const RecentsPage = (props:IRecentProps) => {
+const RecentsPage = (props: IRecentProps) => {
   const [posts, setPosts] = useState<TMusicContent[]>([]);
   const [user, setUser] = useState<TUser | null>(null);
   const [song, setSong] = useState<TMusicContent | null>(null);
@@ -26,16 +27,19 @@ const RecentsPage = (props:IRecentProps) => {
   }, [Session.getUser()]);
   useEffect(() => {
     if (user) {
-      if(service == "spotify") {
-         getSpotifyRecentlyPlayedSongs(user.spotifyToken, 10).then((posts) => {
+      if (service == "spotify") {
+        getSpotifyRecentlyPlayedSongs(user.spotifyToken, 10).then((posts) => {
           setPosts(posts);
-         })
+        });
       } else {
-
+        getAppleRecentlyPlayedSongs(props.appleMusicInstance).then((posts) => {
+          setPosts(posts);
+          console.log(posts);
+        });
       }
     }
   }, [user]);
-  
+
   if (!user) {
     return <div>fetching user</div>;
   }
@@ -44,7 +48,7 @@ const RecentsPage = (props:IRecentProps) => {
     <div className="min-h-screen min-w-screen bg-lblue">
       <Navbar />
       <h1 className="text-4xl text-center text-white fixed-scroll">
-              Recently Played
+        Recently Played
       </h1>
       <div className="grid grid-cols-3">
         <div className="flex items-center justify-center col-span-2 mt-5">
@@ -53,17 +57,17 @@ const RecentsPage = (props:IRecentProps) => {
           </div>
         </div>
         <div className="mt-5">
-        {service === "apple" ? (
-          <AppleMusicPlayerCard
-            musicInstance={props.appleMusicInstance}
-            selectedSong={song}
-          />
-        ) : (
-          <SpotifyPlayerCard
-            selectedSong={song}
-            appleMusicInstance={props.appleMusicInstance}
-          />
-        )}
+          {service === "apple" ? (
+            <AppleMusicPlayerCard
+              musicInstance={props.appleMusicInstance}
+              selectedSong={song}
+            />
+          ) : (
+            <SpotifyPlayerCard
+              selectedSong={song}
+              appleMusicInstance={props.appleMusicInstance}
+            />
+          )}
         </div>
       </div>
     </div>
