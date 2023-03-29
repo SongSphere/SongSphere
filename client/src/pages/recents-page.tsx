831c3092c6f527df3d49427777bbc7010e9142f4
ekane02 <70538006+ekane02@users.noreply.github.com>
2023-03-29 12:44:37 -0400

    finished recently played UI

--- client/src/pages/recents-page.tsx
+++ client/src/pages/recents-page.tsx
@@ -6,23 +6,32 @@ import { useEffect } from "react";
 import { useState } from "react";
 import { TPost } from "../types/post";
 import { TUser } from "../types/user";
-import LikeFeed from "../components/profile/likes-feed";
+import RecentFeed from "../components/profile/recents-feed";
+import AppleMusicPlayerCard from "../components/player/apple-music-player-card";
+import SpotifyPlayerCard from "../components/player/spotify-music-player-card";
 
-const RecentsPage = () => {
+interface IRecentProps {
+  appleMusicInstance: MusicKit.MusicKitInstance;
+}
+
+const RecentsPage = (props:IRecentProps) => {
   const [posts, setPosts] = useState<TPost[]>([]);
   const [user, setUser] = useState<TUser | null>(null);
   const [song, setSong] = useState<TMusicContent | null>(null);
+  const [service, setService] = useState<string>("");
   useEffect(() => {
     setUser(Session.getUser());
+    setService(Session.getMusicService());
   }, [Session.getUser()]);
   useEffect(() => {
     if (user) {
       fetchPostsByUsername(user.username).then((posts) => {
         setPosts(posts);
+        
       });
     }
   }, [user]);
-
+  
   if (!user) {
     return <div>fetching user</div>;
   }
@@ -30,13 +39,27 @@ const RecentsPage = () => {
   return (
     <div className="min-h-screen min-w-screen bg-lblue">
       <Navbar />
-      <div className="flex items-center justify-center mt-5">
-        <div className="w-4/5">
-          <h1 className="text-4xl text-white">
-            Recently Played from{" "}
-            {Session.getMusicService().replace("s", "S").replace("a", "A")}
-          </h1>
-          <LikeFeed posts={posts} user={user} setSong={setSong} />
+      <h1 className="text-4xl text-center text-white fixed-scroll">
+              Recently Played
+      </h1>
+      <div className="grid grid-cols-3">
+        <div className="flex items-center justify-center col-span-2 mt-5">
+          <div className="w-4/5">
+            <RecentFeed posts={posts} user={user} setSong={setSong} />
+          </div>
+        </div>
+        <div className="mt-5">
+        {service === "apple" ? (
+          <AppleMusicPlayerCard
+            musicInstance={props.appleMusicInstance}
+            selectedSong={song}
+          />
+        ) : (
+          <SpotifyPlayerCard
+            selectedSong={song}
+            appleMusicInstance={props.appleMusicInstance}
+          />
+        )}
         </div>
       </div>
     </div>
