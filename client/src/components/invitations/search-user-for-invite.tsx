beae15ec80869fdeede9c287db4662d3e1cecd7b
committ <89640845+spaceComplexitys@users.noreply.github.com>
2023-04-10 17:35:46 -0400

    Fixed Frontend where only one should show up

--- client/src/components/invitations/search-user-for-invite.tsx
+++ client/src/components/invitations/search-user-for-invite.tsx
@@ -18,7 +18,7 @@ const FollowingListForInvite = (props: IFollowingListProps) => {
   };
 
   useEffect(() => {
-    setFollowing(props.following);
+    setFollowing([]);
   }, [props.following]);
 
   if (!props.isVisible) {
@@ -56,57 +56,67 @@ const FollowingListForInvite = (props: IFollowingListProps) => {
                 placeholder="Search Username"
                 onChange={async (event) => {
                   // searching
-                  let filteredUsers: Array<string> = Array<string>();
 
-                  props.following.forEach((u) => {
-                    if (u.startsWith(event.target.value as string)) {
-                      filteredUsers.push(u);
-                    }
-                  });
+                  if ((event.target.value as string) === "") {
+                    setFollowing([]);
+                  } else if ((event.target.value as string) !== "") {
+                    let filteredUsers: Array<string> = Array<string>();
 
-                  setFollowing(filteredUsers);
+                    props.following.forEach((u) => {
+                      if (u.startsWith(event.target.value as string)) {
+                        filteredUsers.push(u);
+                      }
+                    });
+
+                    setFollowing(filteredUsers);
+                  }
                 }}
               />
             </div>
 
             <div className="justify-center py-2 text-center">
               <div className="overflow-y-auto max-h-[45vh]">
-                {following.map((user) => {
-                  return (
-                    <div className="flex">
-                      <div className="flex-1 inline-block text-left">
-                        <div>
-                          <a className="flex items-center p-2 text-base font-normal ">
-                            <svg
-                              className="flex-shrink-0 w-6 h-6"
-                              viewBox="0 0 20 20"
-                              xmlns="http://www.w3.org/2000/svg"
-                            ></svg>
-                            <span className="flex-1 inline-block text-left">
-                              {user}
-                            </span>
-
-                            {isInvited ? (
-                              <button
-                                className="inline-flex items-center justify-center px-2 ml-3 text-sm font-medium text-gray-800 bg-gray-200 rounded-full dark:bg-gray-700 dark:text-gray-300"
-                                onClick={() => handleInviteClick()}
-                              >
-                                Invite
-                              </button>
-                            ) : (
-                              <button
-                                className="inline-flex items-center justify-center px-2 ml-3 text-sm font-medium text-gray-800 bg-gray-200 rounded-full dark:bg-gray-700 dark:text-gray-300"
-                                onClick={() => handleInviteClick()}
-                              >
-                                Invited
-                              </button>
-                            )}
-                          </a>
+                {following.length > 0 ? (
+                  following.map((user) => {
+                    return (
+                      <div className="flex">
+                        <div className="flex-1 inline-block text-left">
+                          <div>
+                            <a className="flex items-center p-2 text-base font-normal ">
+                              <svg
+                                className="flex-shrink-0 w-6 h-6"
+                                viewBox="0 0 20 20"
+                                xmlns="http://www.w3.org/2000/svg"
+                              ></svg>
+                              <span className="flex-1 inline-block text-left">
+                                {user}
+                                {isInvited ? (
+                                  <button
+                                    className="inline-flex items-center justify-center px-2 ml-3 text-sm font-medium text-gray-800 bg-gray-200 rounded-full dark:bg-gray-700 dark:text-gray-300"
+                                    onClick={() => handleInviteClick()}
+                                  >
+                                    Invite
+                                  </button>
+                                ) : (
+                                  <button
+                                    className="inline-flex items-center justify-center px-2 ml-3 text-sm font-medium text-gray-800 bg-gray-200 rounded-full dark:bg-gray-700 dark:text-gray-300"
+                                    onClick={() => handleInviteClick()}
+                                  >
+                                    Invited
+                                  </button>
+                                )}
+                              </span>
+                            </a>
+                          </div>
                         </div>
                       </div>
-                    </div>
-                  );
-                })}
+                    );
+                  })
+                ) : (
+                  <div className="px-3 py-2 cursor-pointer hover:bg-slate-100">
+                    <p className="text-sm font-medium text-gray-600"></p>
+                  </div>
+                )}
               </div>
             </div>
           </div>
