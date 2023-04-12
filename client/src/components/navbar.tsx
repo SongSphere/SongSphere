311ee3a37377508729cc1474897015a799f6a02a
Chi-Wei Lien <wilsonwilson3124@gmail.com>
2023-04-12 15:38:04 -0400

    highlight the current page in navbar

--- client/src/components/navbar.tsx
+++ client/src/components/navbar.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import handleSignout from "../services/user/handle-sign-out";
 import Session from "../session";
 
@@ -9,6 +9,8 @@ interface INavbarProps {}
 
 const Navbar = (props: INavbarProps) => {
   const [mobileNavOpen, setMobileNavOpen] = useState(false);
+  const active = window.location.href.split("/")[3];
+
   return (
     <nav className="shadow-lg bg-slate-800">
       <div className="w-screen px-4 mx-auto">
@@ -24,46 +26,121 @@ const Navbar = (props: INavbarProps) => {
             </div>
             {/* primary navbar items */}
             <div className="items-center hidden space-x-1 lg:flex">
-              <Link to="/">
-                <div className="px-2 py-4 font-semibold border-b-4 border-sky-300 text-sky-300">
-                  Home
-                </div>
-              </Link>
-              <Link to="/followRequest">
-                <div className="px-2 py-4 font-semibold text-slate-200 border-sky-300 hover:text-sky-300">
-                  Follow Request
-                </div>
-              </Link>
-
-              <Link to="/profile">
-                <div className="px-2 py-4 font-semibold text-slate-200 border-sky-300 hover:text-sky-300">
-                  Profile
-                </div>
-              </Link>
-              <Link to="/searchUsers">
-                <div className="px-2 py-4 font-semibold text-slate-200 border-sky-300 hover:text-sky-300">
-                  Search
+              <Link
+                to="/"
+                className="font-semibold text-slate-200 hover:text-sky-300"
+              >
+                <div
+                  className={
+                    active == "" ? "border-b-4 border-sky-300 text-sky-300" : ""
+                  }
+                >
+                  <div className="px-2 py-4">Home</div>
                 </div>
               </Link>
 
-              <Link to="/posts">
-                <div className="px-2 py-4 font-semibold text-slate-200 border-sky-300 hover:text-sky-300">
-                  Post
+              <Link
+                to="/followRequest"
+                className="font-semibold text-slate-200 hover:text-sky-300"
+              >
+                <div
+                  className={
+                    active == "followRequest"
+                      ? "border-b-4 border-sky-300 text-sky-300"
+                      : ""
+                  }
+                >
+                  <div className="px-2 py-4">Follow Request</div>
                 </div>
               </Link>
-              <Link to="/notificationsPage">
-                <div className="px-2 py-4 font-semibold text-slate-200 border-sky-300 hover:text-sky-300">
-                  Notification
+
+              <Link
+                to="/profile"
+                className="font-semibold text-slate-200 hover:text-sky-300"
+              >
+                <div
+                  className={
+                    active == "profile"
+                      ? "border-b-4 border-sky-300 text-sky-300"
+                      : ""
+                  }
+                >
+                  <div className="px-2 py-4">Profile</div>
                 </div>
               </Link>
-              <Link to="/createRoom">
-                <div className="px-2 py-4 font-semibold text-slate-200 border-sky-300 hover:text-sky-300">
-                  Party Room
+
+              <Link
+                to="/searchUsers"
+                className="font-semibold text-slate-200 hover:text-sky-300"
+              >
+                <div
+                  className={
+                    active == "searchUsers"
+                      ? "border-b-4 border-sky-300 text-sky-300"
+                      : ""
+                  }
+                >
+                  <div className="px-2 py-4">Search</div>
                 </div>
               </Link>
-              <Link to="/settings">
-                <div className="px-2 py-4 font-semibold text-slate-200 border-sky-300 hover:text-sky-300">
-                  Settings
+
+              <Link
+                to="/posts"
+                className="font-semibold text-slate-200 hover:text-sky-300"
+              >
+                <div
+                  className={
+                    active == "posts"
+                      ? "border-b-4 border-sky-300 text-sky-300"
+                      : ""
+                  }
+                >
+                  <div className="px-2 py-4">Post</div>
+                </div>
+              </Link>
+
+              <Link
+                to="/notificationsPage"
+                className="font-semibold text-slate-200 hover:text-sky-300"
+              >
+                <div
+                  className={
+                    active == "notificationsPage"
+                      ? "border-b-4 border-sky-300 text-sky-300"
+                      : ""
+                  }
+                >
+                  <div className="px-2 py-4">Notification</div>
+                </div>
+              </Link>
+
+              <Link
+                to="/createRoom"
+                className="font-semibold text-slate-200 hover:text-sky-300"
+              >
+                <div
+                  className={
+                    active == "createRoom"
+                      ? "border-b-4 border-sky-300 text-sky-300"
+                      : ""
+                  }
+                >
+                  <div className="px-2 py-4">Party Room</div>
+                </div>
+              </Link>
+
+              <Link
+                to="/settings"
+                className="font-semibold text-slate-200 hover:text-sky-300"
+              >
+                <div
+                  className={
+                    active == "settings"
+                      ? "border-b-4 border-sky-300 text-sky-300"
+                      : ""
+                  }
+                >
+                  <div className="px-2 py-4">Settings</div>
                 </div>
               </Link>
             </div>
