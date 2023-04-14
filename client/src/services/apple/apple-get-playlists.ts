b3bf2c548a9e431614aa2683b01d794c607afa59
anthonyfmb <anthony.baumann@icloud.com>
2023-04-13 20:29:22 -0400

    finish apple playlist

--- client/src/services/apple/apple-get-playlists.ts
+++ client/src/services/apple/apple-get-playlists.ts
@@ -12,7 +12,7 @@ export const getApplePlaylists = async (
   await musicInstance.api.library
     .playlists(null, {
       limit: limit,
-      include: ["library-songs", "artwork"],
+      relationships: "tracks",
     })
     .then((response) => {
       // Apple Music's search doesn't filter for types, possibly because MusicKit.getInstance().api.search
@@ -27,7 +27,10 @@ export const getApplePlaylists = async (
 
       // if (contents === undefined) return [];
 
-      response.forEach(function (entry: any) {
+      response.forEach(async function (entry: any) {
+        await musicInstance.api.library
+          .playlist(entry.attributes.id, {})
+          .then((response) => {});
         list.push({
           name: entry.attributes.name,
           id: entry.id,
@@ -45,4 +48,23 @@ export const getApplePlaylists = async (
 export const getAppleTracksFromPlaylist = async (
   id: string,
   musicInstance: MusicKit.MusicKitInstance
-) => {};
+) => {
+  let songs: TMusicContent[] = [];
+
+  await musicInstance.api.library.playlist(id, {}).then((response) => {
+    response.relationships.tracks.data.forEach(function (entry: any) {
+      songs.push({
+        name: entry.attributes.name,
+        artist: entry.attributes.artistName,
+        albumName: entry.attributes.albumName,
+        id: entry.id,
+        service: "apple",
+        category: "song",
+        cover: entry.attributes.artwork.url
+          .replace("{w}", 1000)
+          .replace("{h}", 1000),
+      });
+    });
+  });
+  console.log(songs);
+};
