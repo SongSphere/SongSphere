8eb0878e415f337b9fa0dbdb45c52c43723744bb
anthonyfmb <anthony.baumann@icloud.com>
2023-04-14 13:18:11 -0400

    small change

--- client/src/services/apple/apple-get-playlists.ts
+++ client/src/services/apple/apple-get-playlists.ts
@@ -3,34 +3,28 @@ import { textChangeRangeNewSpan } from "typescript";
 import { TMusicContent } from "../../types/music-content";
 import { TPlaylist } from "../../types/playlist";
 
+// Use a playlist's ID to access its songs
+// MusicKit doesn't provide cover or # of songs
+
 export const getApplePlaylists = async (
   limit: number,
   musicInstance: MusicKit.MusicKitInstance
 ) => {
+  console.log("start");
   const list: TPlaylist[] = [];
 
   await musicInstance.api.library
     .playlists(null, {
       limit: limit,
-      relationships: "tracks",
+      //relationships: "tracks",
     })
     .then((response) => {
-      // Apple Music's search doesn't filter for types, possibly because MusicKit.getInstance().api.search
-      // is depricated, I just implemented it here
-
-      console.log(response);
-      // const contents = Object.values(response).find((entry) => {
-      //   if (entry.data[0].type === types) {
-      //     return entry;
-      //   }
-      // });
-
-      // if (contents === undefined) return [];
-
       response.forEach(async function (entry: any) {
-        await musicInstance.api.library
-          .playlist(entry.attributes.id, {})
-          .then((response) => {});
+        // const response = await musicInstance.api.library.playlist(
+        //   entry.attributes.id,
+        //   {}
+        // );
+        // console.log(response);
         list.push({
           name: entry.attributes.name,
           id: entry.id,
