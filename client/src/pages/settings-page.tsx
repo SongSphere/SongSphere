1ba1248bfb7361678f0a7f302d48593c9e66698c
committ <89640845+spaceComplexitys@users.noreply.github.com>
2023-04-17 12:59:36 -0400

    Moving test cases

--- client/src/pages/settings-page.tsx
+++ client/src/pages/settings-page.tsx
@@ -2,12 +2,11 @@ import { useEffect, useState } from "react";
 import { TUser } from "../types/user";
 import Session from "../session";
 import BlockedList from "../components/settings/blocked-list";
-import ProfileSettingCard from "../components/settings/profile-setting-card";
-
 import EvenLayout from "../layouts/even-layout";
 import ImageSettingCard from "../components/settings/image-setting-card";
 import MusicSettingCard from "../components/settings/music-setting-card";
 import DangerZone from "../components/settings/danger-zone";
+import ProfileSettingCard from "../components/settings/profile-setting-card";
 
 const SettingsPage = () => {
   const [user, setUser] = useState<TUser | null>(null);
