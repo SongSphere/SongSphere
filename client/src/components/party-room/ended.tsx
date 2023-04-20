9bb84bbb6bb3b7de8d85e6844745831913273ef1
ekane02 <70538006+ekane02@users.noreply.github.com>
2023-04-19 22:45:32 -0400

    fix popup

--- client/src/components/party-room/ended.tsx
+++ client/src/components/party-room/ended.tsx
@@ -16,9 +16,6 @@ const Ended = () => {
       <h1 className="text-4xl font-extrabold tracking-widest text-white">
       This party has been ended by the owner
       </h1>
-      <div className="bg-[#FF6A3D] px-2 text-sm rounded rotate-12 absolute">
-        
-      </div>
       <button className="mt-5">
         <a className="relative inline-block text-sm font-medium text-white group active:text-yellow-300 focus:outline-none focus:ring">
           <span className="absolute inset-0 transition-transform translate-x-0.5 translate-y-0.5 bg-white group-hover:translate-y-0 group-hover:translate-x-0"></span>
